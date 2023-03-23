586db6f4f58932477822adc142c8270bb2bd324f
CrossOrigin88 <crossorigin88@gmail.com>
2023-03-23 21:55:28 +0700

    working with 'pug' template engine

--- routes/admin.js
+++ routes/admin.js
@@ -6,14 +6,17 @@ const rootDir = require('../utils/path');
 
 const router = express.Router();
 
+const products = [];
+
 router.get('/add-product', (req, res, next) => {
-    console.log('add product page!');
-    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+    res.render('add-product', {pageTitle: 'Add Product', path:'/admin/add-product'});
 });
 
 router.post('/add-product', (req, res, next) => {
-    console.log(req.body);
+    products.push({title: req.body.title})
+    console.log(products);
     res.redirect('/');
 });
 
-module.exports = router;
+exports.router = router;
+exports.products = products
